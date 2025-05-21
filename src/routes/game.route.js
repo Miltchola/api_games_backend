de6952702ae364263fda50d3c376c80af8b94047
PedroHenriqueAF <phafonseca2003@gmail.com>
2025-05-21 09:36:52 -0300

    0.3.1
    JOGOS DA API NO BANCO FUNFANDO

--- src/routes/game.route.js
+++ src/routes/game.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import gameController from '../controller/game.controller.js';
+import rawgController from '../controller/rawg.controller.js';
 
 const router = express.Router();
 
@@ -7,6 +8,6 @@ router.post('/', gameController.create);
 router.get('/', gameController.getAll);
 
 // Rota para importar jogos da RAWG
-router.post('/import-rawg', gameController.importFromRawg);
+router.post('/import-rawg', rawgController.importFromRawg);
 
 export default router;
